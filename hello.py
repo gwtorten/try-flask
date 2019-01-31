a871f45a8b3db2991139fac336002189fd04cdb7
gwtorten <47228941+gwtorten@users.noreply.github.com>
2019-01-31 14:30:04 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Gil! lmao'
+    return 'Hello, Gil!'
 
 #use $env:FLASK_APP="hello,py" and then flask run. This is in powershell NOT terminal.
