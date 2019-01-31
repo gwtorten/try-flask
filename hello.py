727b7b2ca2a7761253d0b13cb6ff964928f32061
gwtorten <47228941+gwtorten@users.noreply.github.com>
2019-01-31 14:31:27 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,4 +5,19 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Gil!'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
+
 #use $env:FLASK_APP="hello,py" and then flask run. This is in powershell NOT terminal.
